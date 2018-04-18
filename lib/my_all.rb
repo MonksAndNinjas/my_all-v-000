76f28177ac7bf6a0e6a77c79da1eee76acea793b
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-18 17:56:29 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,10 +4,7 @@ def my_all?(collection)
   i = 0
 
   while i < collection.length
-    block_return_values << yield(collection[i])
+    yield(collection[i])
     i += 1
   end
-end
-
-my_all?(collection) do |value|
 end
